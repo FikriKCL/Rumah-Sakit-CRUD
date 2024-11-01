a6da3e3483df6f99dd0dce0961b29063057a9477
Fikri Alfathir <111261686+FikriKCL@users.noreply.github.com>
2024-11-01 15:09:39 +0700

    init

---
+++ projek.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <iomanip>  
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+int main() {
+cout <<"                                        Selamat Datang" << endl;
+
+this_thread::sleep_for(2000ms);
+std::string judul = R"(
+  _____                           _        _____         _     _  _     _    _  _____  _____ 
+ |  __ \                         | |      / ____|       | |   (_)| |   | |  | ||  __ \|_   _|
+ | |__) |_   _  _ __ ___    __ _ | |__   | (___    __ _ | | __ _ | |_  | |  | || |__) | | |  
+ |  _  /| | | || '_ ` _ \  / _` || '_ \   \___ \  / _` || |/ /| || __| | |  | ||  ___/  | |  
+ | | \ \| |_| || | | | | || (_| || | | |  ____) || (_| ||   < | || |_  | |__| || |     _| |_ 
+ |_|  \_\\__,_||_| |_| |_| \__,_||_| |_| |_____/  \__,_||_|\_\|_| \__|  \____/ |_|    |_____|
+                                                                                             
+                                                                                             
+)";
+
+    for(int i = 0; i <= 200; i++){
+
+    if(i == 100){
+        cout << judul;
+    }else{
+        cout << "=";
+     }
+    }
+
+    vector <string> penyakit = {"Demam Berdarah", "HIV"};
+    // for(int i = 0;){
+
+    // }
+}
