a427904bf9d2d453c31d01a861295cf474f91140
Hnfmzfrgnwn <hmuzafargunawan@gmail.com>
2024-11-12 19:43:41 +0700

    Fitur tambahan = pilihan poli, jadwal, dll

--- projek.cpp
+++ projek.cpp
@@ -4,8 +4,8 @@
 #include <thread>
 #include <vector>
 #include <fstream>
-#include <nlohmann/json.hpp>
-using json = nlohmann::json;
+//#include <nlohmann/json.hpp>
+//using json = nlohmann::json;
 
 using namespace std;
 
@@ -22,46 +22,96 @@ void menu() {
  |_|  \_\\__,_||_| |_| |_| \__,_||_| |_| |_____/  \__,_||_|\_\|_| \__|  \____/ |_|    |_____|
  )";
 
-    for(int i = 0; i <= 200; i++){
-        if(i == 100){
-            for (char ch : judul) {
-                cout << ch ;                     
-                cout.flush();                    
-                this_thread::sleep_for(1ms);    
-            }
-        }else if( i == 200){
-            cout << endl;
-        } else {
-            cout << "=";
-        }
-        this_thread::sleep_for(1ms);
-    }
+    //for(int i = 0; i <= 200; i++){
+    //    if(i == 100){
+    //        for (char ch : judul) {
+    //            cout << ch ;                     
+    //            cout.flush();                    
+    //            this_thread::sleep_for(1ms);    
+    //        }
+    //    }else if( i == 200){
+    //        cout << endl;
+    //    } else {
+    //        cout << "=";
+    //    }
+    //    this_thread::sleep_for(1ms);
+    //}
   
 }
 
 void pendaftaran() {
     string nama, nik, umur, jenis_kelamin, ttl;
 
-    cout << " Masukan Nama : "; getline(cin,nama);
-    cout << " Masukan NIK : "; getline(cin,nik);
-    cout << " Masukan Umur : "; getline(cin,umur);
-    cout << " Masukan Jenis Kelamin : "; getline(cin,jenis_kelamin);
+    cout << " Masukan Nama                               : "; getline(cin,nama);
+    cout << " Masukan NIK                                : "; getline(cin,nik);
+    cout << " Masukan Umur                               : "; getline(cin,umur);
+    cout << " Masukan Jenis Kelamin                      : "; getline(cin,jenis_kelamin);
     cout << " Masukan Tempat Tanggal Lahir [DD/~MM/YYYY] : "; getline(cin,ttl);
 
 }
 
-void poliGigi(){
-
+void poliGigi(int PG){
+    int jadwal, Kunjungan, dokter,ketik;
+        switch (PG)
+        {
+            case 1:
+                cout<<" POLI GIGI "<<endl;
+                cout<<"==========="<<endl;
+                cout<<" Ketik 1 = Jadwal Perawatan"<<endl;
+                cout<<" Ketik 2 = Daftar Dokter"<<endl;
+                //cout<<" ketik 3 = "
+                cout<<" ========================== "<<endl;
+                cout<<" PILIH :  ";cin>>ketik;
+                
+                    if (ketik==1)
+                    {
+                        cout<<" JADWAL PERAWATAN  "<<endl;
+                        cout<<"=================="<<endl;
+                        cout<<" - Senin   : 08.00 - 12.00 "<<endl;
+                        cout<<" - Selasa  : 13.00 - 17.00 "<<endl;
+                        cout<<" - Rabu    : 08.00 - 12.00 "<<endl;
+                        cout<<" - Kamis   : 13.00 - 17.00 "<<endl;
+                        cout<<" - Jumat   : 08.00 - 12.00 "<<endl;
+                        break;
+                    }
+                    else if (ketik==2)
+                    {
+                        cout<<" - Senin   : Dr. Hamdoko Sujatmiko Mrs "<<endl;
+                        break;
+                    }
+            break;
+        default:
+            cout<<" SILAKAN ULANG KEMBALI "<<endl;
+            break;
+        }
 }
 
 void pembayaran(){
-    
+    int jadwal, pembayaran;
 }
 
 
 int main() {
+    int PG,ketik;
     menu();
     pendaftaran();
+
+            cout<<" SILAKAN PILIH POLI YANG DIINGINKAN "<<endl;
+        cout<<"===================================="<<endl;
+        cout<<" - POLI GIGI    = 1 "<<endl;
+        cout<<" - POLI ANAK    = 2  "<<endl;
+        cout<<" - POLI JANTUNG = 3 "<<endl;
+
+        cout<<"PILIH = ";cin>>PG;
+        if (PG)
+        {
+           poliGigi(PG); 
+        }
+        else
+        {
+            
+        }
+        
     // for(int i = 0;){
 
     // }
